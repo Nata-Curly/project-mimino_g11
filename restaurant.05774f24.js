c2a65aebccfe26527dcb28c49546770380c2a14e
Nata-Curly <117598305+Nata-Curly@users.noreply.github.com>
2023-01-29 20:43:51 +0000

    Deploying to gh-pages from @ Nata-Curly/project-mimino_g11@0dfa66dafb2efd5070188b6ca8ea4ab807db7e47 🚀

--- restaurant.05774f24.js
+++
@@ -1,2 +0,0 @@
-function customSelect(){var e=document.querySelectorAll(".js-select");e.length>0&&e.forEach((function(e){NiceSelect.bind(e,{searchable:!0,placeholder:"select",searchtext:"zoek",selectedtext:"geselecteerd"})}))}customSelect();
-//# sourceMappingURL=restaurant.05774f24.js.map
--- restaurant.3ca95d73.js
+++
@@ -1,2 +0,0 @@
-function inputMask(){var n=document.querySelectorAll(".js-inputmask"),a=document.querySelectorAll(".js-inputmask-01");n.length>0&&n.forEach((function(n){IMask(n,{mask:"+{38}(000)000-00-00",lazy:!1})})),a.length>0&&a.forEach((function(n){IMask(n,{mask:"+{80}(000)000-00-00",lazy:!1})}))}inputMask();
-//# sourceMappingURL=restaurant.3ca95d73.js.map
