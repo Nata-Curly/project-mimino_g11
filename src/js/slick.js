e532bd3686707802be8307d23961956b26723993
Dmytro Yermolenko <dima.ermol22@gmail.com>
2023-01-31 01:00:48 +0200

    Final section

--- src/js/slick.js
+++ src/js/slick.js
@@ -9,18 +9,18 @@ $('.responsive').slick({
     slidesToShow: 2,
   
     responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
+      // {
+      //   breakpoint: 1200,
+      //   settings: {
+      //     slidesToShow: 2,
+      //   },
+      // },
+      // {
+      //   breakpoint: 768,
+      //   settings: {
+      //     slidesToShow: 1,
+      //   },
+      // },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
